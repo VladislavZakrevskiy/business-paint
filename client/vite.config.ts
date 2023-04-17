3111b0f4cdb2802cf3dc27d06ff687bce5519322
vlad <zakrevskiyvlada@yandex.ru>
2023-04-17 19:23:36 +0400

    and and

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.ts','.mjs']
+    extensions: ['.js', '.ts','.mjs', '.jsx', 'tsx']
   }
 })
